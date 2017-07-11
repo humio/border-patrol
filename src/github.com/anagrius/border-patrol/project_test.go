2461303d4306f205112ad12e09a3fb53bf149859
Thomas Anagrius <thb@trifork.com>
2017-07-11 19:43:47 +0200

    Fix local path in tests

--- src/github.com/anagrius/border-patrol/project_test.go
+++ src/github.com/anagrius/border-patrol/project_test.go
@@ -1,22 +1,31 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"path"
+	"testing"
+)
+
+func getDir() string {
+	wd, _ := os.Getwd()
+	return wd
+}
 
 func TestParsingModules(t *testing.T) {
-	project := readProject("/Users/thb/Code/border-patrol/test/elm", "elm")
+	project := readProject(path.Join(getDir(), "test", "elm"), "elm")
 	expected := []string{"Api.Z", "Core.Y", "Util.X", "Main"}
 	expectListsEq(t, sources(project), expected)
 }
 
 func TestParsingImports(t *testing.T) {
-	project := readProject("/Users/thb/Code/border-patrol/test/elm", "elm")
+	project := readProject(path.Join(getDir(), "test", "elm"), "elm")
 	imports := importsBySource("Core.Y", project)
 	expected := []string{"Html", "Html.Attributes", "Api.Z", "Util.X"}
 	expectListsEq(t, imports, expected)
 }
 
 func TestParsingScala(t *testing.T) {
-	rootDir := "/Users/thb/Code/border-patrol/test/scala"
+	rootDir := path.Join(getDir(), "test", "scala")
 	project := readProject(rootDir, "scala")
 	imports := importsBySource("com.humio.core", project)
 	expected := []string{"bar.xi.moo.X", "java.lang._", "com.humio.kafka._"}
